01f912580401d0ce01a250c08c512ea2bb99c999
Brian Pena <mr.penabrian@hotmail.com>
2023-11-17 13:25:39 -0500

    add inRange method

--- _.js
+++ _.js
@@ -4,6 +4,26 @@ const _ = {
       const clampedValue = Math.min(lowerClampedValue, upper);
       return clampedValue;
     },
+    inRange(number, startVal, endVal) {
+      if (endVal === undefined) {
+          endVal = startVal;
+          startVal = 0;
+        }
+        if (startVal > endVal) {
+          [startVal, endVal] = [endVal, startVal]
+        }
+      if (number < startVal) {
+        return false;
+        }
+      if (number >= endVal) {
+          return false;
+        }
+        if (number >= startVal && number <= endVal) {
+          return true;
+        } else {
+          return false;
+        }
+      },
   };
 
 
