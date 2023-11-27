617f81dcc90222c5985708c7b08a111cbd99544f
Brian Pena <mr.penabrian@hotmail.com>
2023-11-27 12:56:35 -0500

    add findKey method

--- _.js
+++ _.js
@@ -39,6 +39,16 @@ const _ = {
         invertedObject[originalValue] = key
       }
       return invertedObject;
+    },
+    findKey(object, predicate) {
+      for (let key in object) {
+        let value = object[key];
+        let predicateReturnValue = predicate(value);
+        if (predicateReturnValue) {
+          return key;
+        }
+      }
+      return undefined;
     }
   };
 
